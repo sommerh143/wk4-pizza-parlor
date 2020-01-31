c1ce082e7c973aff0196e1f9507166a88872326d
sh <sommerh143@yahoo.com>
2020-01-31 11:25:32 -0800

    add the pizza size to a constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,42 @@
+function AllOrders () {
+  this.orders = [],
+  this.currentId = 0
+}
+
+AllOrders.prototype.addOrder = function(order) {
+  order.id = this.assignId();
+  this.contacts.push(order);
+}
+
+function Order(size, topping){
+  this.size = size,
+  this.topping = topping
+}
 
 
-
-
-
-
+var allOrders = new AllOrders();
 
 $(document).ready(function () {
   $("#submitName").click(function(event){
     event.preventDefault();
-    var name = $("#coustomerName").val();
-    $("#inputName").text(name);
+    var inputtedName = $("#coustomerName").val();
+    $("#inputName").text(inputtedName);
+    console.log(inputtedName);
   })
 
-  var toppingPrice = [];
- $("#toppingBtn").click(function(){
-  $("input:checkbox[name=topping]:checked").each(function(){
-    var toppingResults = $(this).val();
-    toppingPrice.push(toppingResults);
+  $("#toppingBtn").click(function(){
+    var inputtedSize = $("#pizzaSize").val();
+    $("#pizzaSize").val("");
+    
+    var newOrder = new Order(inputtedSize);
+    console.log(newOrder);
+    $("input:checkbox[name=topping]:checked").each(function(){
+      
+      var toppingResults = $(this).val();
+      // topping.push(toppingResults);
+      $(topping).val([]);
+      console.log(newOrder);
+    $()
    });
   });
 });
