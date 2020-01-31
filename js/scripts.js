66cb1a84acf1a95b46f8c1dc6c678d79afe45e5b
sh <sommerh143@yahoo.com>
2020-01-31 09:57:03 -0800

    will let coustomer input topping flavers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
+
+
+
+
+
+
+
 $(document).ready(function () {
   $("#submitName").click(function(event){
     event.preventDefault();
     var name = $("#coustomerName").val();
     $("#inputName").text(name);
   })
+
+  var toppingPrice = [];
+ $("#toppingBtn").click(function(){
+  $("input:checkbox[name=topping]:checked").each(function(){
+    var toppingResults = $(this).val();
+    toppingPrice.push(toppingResults);
+   });
+  });
 });
